ea03d28f0fefa5c206518a303a2986828805d697
RayHE <rayhe88@gmail.com>
2023-01-03 17:04:35 -0600

    Indications for installing like a package

--- parserQE/__init__.py
+++ parserQE/__init__.py
@@ -1,6 +1,6 @@
 from os.path import dirname, abspath
 
-from parserQE.parser import loadQE, dumpXYZ
+from parserQE.parser import loadQE, dumpXYZ, printQE_info
 
 ROOT_DIR = dirname(abspath(__file__))
 
--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(name="parserQE", package=['parserQE'])
+VERSION = '0.0.0'
+DESCRIPTION = 'Parser for Quantum Espresso'
+LONG_DESCRIPTION = 'My python package for Quantum Espresso, made in ANL'
+
+# Setting up
+setup(
+        name="parserQE",
+        version=VERSION,
+        author="RayHEsparza",
+        author_email="rayhe88@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['QuantumEspresso','Parser'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: Linux"
+            ] 
+    )
